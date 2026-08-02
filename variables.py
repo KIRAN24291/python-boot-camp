848c28ff817ca9ecc8d405266942f7fd27f6aaca
KIRAN24291 <kiran242919@gmail.com>
2026-08-02 19:22:04 +0530

    variable

---
+++ variables.py
@@ -0,0 +1,14 @@
+'''
+A variable is a container that stores data.
+It refers to a memory location where the value is kept.
+You can use variables to store and reuse information in your program.
+Variables cannot start with numbers or any any special symbols , it can start with underscore(_).
+Example: name = "Alice"
+Here = is the assignment operator which assigns the data to the variable name.
+'''
+
+Name = "kiran"          # Double/Single quotes are used to denote the String.
+print(Name)             # print is an inbuilt function which is used to show on the output screen.
+Age = 20                # integers dont need any symbols to denote.
+Height = 150.1          
+print(Name,Age,Height)  
