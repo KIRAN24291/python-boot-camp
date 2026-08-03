15df8b8d0bdf5fbe3232b85ba573fcf654f7785d
KIRAN24291 <kiran242919@gmail.com>
2026-08-03 19:37:22 +0530

    Comments

---
+++ 01_Comments.py
@@ -0,0 +1,10 @@
+'''
+Comments are the non exectable lines of text used to explain the logic or the meaning of the message.
+Commetns are of two types --> single and multi line comments.
+Single line comments are represented with # at the beginning of the text.
+Multi line commetnts are represented with """ at the beginning and at the end of the text.
+Like this:
+"""
+Hello world!
+"""
+'''
