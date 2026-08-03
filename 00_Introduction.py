7183d6888e73c0681d24c5af2d2a6e52242cf0a4
KIRAN24291 <kiran242919@gmail.com>
2026-08-03 19:31:08 +0530

    Introduction

---
+++ 00_Introduction.py
@@ -0,0 +1,6 @@
+'''
+Python is a high-level, interpreted, general-purpose programming language created by Guido van Rossum and first released in 1991.
+It was designed with one primary goal: to make programming simple, readable, and productive.
+Python uses a clean and straightforward syntax that resembles the English language.
+Python is used in many different fields: Web Development, Artificial Intelligence, Data Science, Automation, Cybersecurity, Game Development...
+'''
