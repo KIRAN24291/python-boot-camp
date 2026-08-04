f71336f5bb89d144733891e650538e4b72fd4a40
KIRAN24291 <kiran242919@gmail.com>
2026-08-04 23:14:07 +0530

    intro

---
+++ 02 _Data_types.py
@@ -0,0 +1,18 @@
+'''
+Data types in Python define the type of values stored in a variable.
+Numeric Type
+int --> whole numbers (0,50,-89)
+float --> decimal numbers (58.2,871,-246.0)
+complex --> real and imaginary (a+3b,b+aj)
+'''
+num = 10
+height = 154.5
+math = 4+3j
+
+'''
+Boolean data type represents one of two valaues:True or False
+'''
+'''
+1) String: It is used to store text type of Data.Created with single or double quotes.
+2) 
+'''
