35c599f82eeef3778ac7ced9d292d6aea6b7f5a8
KIRAN24291 <kiran242919@gmail.com>
2026-08-07 23:09:03 +0530

    Operators

---
+++ 06_Operators.py
@@ -0,0 +1,22 @@
+'''
+Operators are used to perform operations on variables and values.
+
+Different types of Operators are:
+1)Arithmetic Operators.
+2)Assignment Operators.
+3)Comparision Operators.
+4)Logical Operators.
+5)Identity Operators.
+6)Membership Operators.
+7)Bitwise Operators.
+
+'''
+
+"""
+Before going into Operators lets understand about Operator Precendence. 
+--> Operator precedence describes the order in which operations are performed.
+-->paranthesis() > exponent** > Mutliplication* or Division/ or modulus% > Addition+ or Subtraction-. 
+Now lets see with an example.
+"""
+result = 10+5*2   
+print(result)
